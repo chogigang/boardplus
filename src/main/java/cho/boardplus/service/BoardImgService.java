c6734b8164dc523804f157a3e4e5b0e4e1c12da5
Lenovo <cbh2031@gmail.com>
2023-04-20 08:38:32 +0900

    파일 업로드 구글링 및 중간고사 시험 준비4일차

--- src/main/java/cho/boardplus/service/BoardImgService.java
+++ src/main/java/cho/boardplus/service/BoardImgService.java
@@ -7,22 +7,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class BoardImgService {
 
-    @Value("{image_fileUploadPath}")
+    @Value("${image_fileUploadPath}")
     private String image_fileUploadPath;
     private final BoardFileRepository boardFileRepository;
     private  final BoardFileService boardFileService;
 
 
-    public void saveBoardImg(BoardFileEntity boardFileEntity )throws Exception{
+    public void saveBoardImg(BoardFileEntity boardFileEntity,MultipartFile boardImgFile )throws Exception{
 
-            String oriImgName = boardFileEntity.getOriginalFileName();
+            String oriImgName = boardImgFile.getOriginalFilename();
+            String imgName= "";
+            String imgUrl = "";
+
+
+            //이미지,파일  업로드
+        if(!StringUtils.isEmpty(oriImgName)){
+            imgName=boardFileService.uploadFile(image_fileUploadPath,oriImgName,
+                    boardImgFile.getBytes());
+            imgUrl= "/images/board/" +imgName;
+        }
+        // 정보 저장
+        boardFileEntity.updateBoardFile(oriImgName,imgName,imgUrl);
+        boardFileRepository.save(boardFileEntity);
 
     }
 
-}
+ }
