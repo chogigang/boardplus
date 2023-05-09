1f3c5ab1a43512c72501b6c844407ea46f2d6504
Lenovo <cbh2031@gmail.com>
2023-05-10 05:59:41 +0900

    아니 왜 뷰단에서 안보이냐...

--- src/main/java/cho/boardplus/confing/WebMvcConfig.java
+++ src/main/java/cho/boardplus/confing/WebMvcConfig.java
@@ -7,16 +7,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Value("${uploadPath}")
+//    String uploadPath;
+//
+//    @Override
+//    public void  addResourceHandlers(ResourceHandlerRegistry registry){
+//        registry.addResourceHandler("/images/**")
+//                .addResourceLocations(uploadPath);
+//    }
 
-    @Value("${uploadPath}")
-    String uploadPath;
+
+
+    private String savePath = "file:///C:/board/"; // 실제 파일 저장 경로(win)
+    private String resourcePath = "/upload/**"; //view 에서 접근할 경로
 
     @Override
-    public void  addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations(uploadPath);
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcePath)
+                .addResourceLocations(savePath);
     }
-
-
-
 }
